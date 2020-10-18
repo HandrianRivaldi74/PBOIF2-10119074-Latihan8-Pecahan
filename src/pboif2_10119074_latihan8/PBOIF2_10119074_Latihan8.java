6a13e68007b346273266c4e3273fe1490c1bd6ef
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-18 14:22:56 +0700

    Latihan7

---
+++ src/pboif2_10119074_latihan8/PBOIF2_10119074_Latihan8.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan8;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskrifsi : Pecahan
+ */
+public class PBOIF2_10119074_Latihan8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.1234567890;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
